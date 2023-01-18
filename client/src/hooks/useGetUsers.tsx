55ed51709e4be6cbe58a89ad63fc57779ebba09c
jingsong.xiong <874470891@qq.com>
2023-01-18 12:13:42 +0800

    Patch: optimization

--- client/src/hooks/useGetUsers.tsx
+++ client/src/hooks/useGetUsers.tsx
@@ -1,19 +1,19 @@
 import {useQuery} from 'react-query';
 import axios from 'axios';
 import {getUsers} from '../constant/requestUrls';
-import {User} from '../component/MembersContent';
 import React from 'react';
+import {UserType} from '../component/Header';
 
 type UseGetUsers = {
   id?: string;
-  onSuccess: React.Dispatch<React.SetStateAction<User[]>>;
+  onSuccess: React.Dispatch<React.SetStateAction<UserType[]>>;
 };
 
 export const useGetUsers = ({id, onSuccess}: UseGetUsers) => {
   return useQuery(
     ['useGetUsers'],
     async () => {
-      return await axios.get(`${getUsers}/${id}`).then((res) => res.data.users as User[]);
+      return await axios.get(`${getUsers}/${id}`).then((res) => res.data.users as UserType[]);
     },
     {
       enabled: !!id,
